40c1b9901b2ae2461f51af7b7f70a16584abc561
romanmatweew <roma.matweew2003@gmail.com>
2024-02-24 09:50:55 +0300

    до 15

---
+++ 13-14-15.py
@@ -0,0 +1,22 @@
+#Задание 13
+if 2**2 >4:
+    print("yes")
+else:
+    print("NO")
+
+#Задание 14
+if 10>100:
+    print ("yes")
+else:
+    print('nope')
+
+#Задание 15
+a = 4
+if a/2 > 0:
+    print('1')
+elif a==4:
+    print ('2')
+elif a <0:
+    print('3')
+else:
+      print('4')
